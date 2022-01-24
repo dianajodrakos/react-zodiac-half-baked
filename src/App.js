4eb9ad322ef57e7b450241cb373f89e67ec0c9d2
DJ Drakos <dianajodrakos@gmail.com>
2022-01-24 14:43:59 -0800

    render Header and Footer props

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
   return (
     <div className="App">
       {/* import and use the Header. Main, and Footer components here */}
-      <Header />
+      <Header name="November '21 Cohort" />
       <Main />
-      <Footer />
+      <Footer year="2022" />
       {/* the Header component needs a string passed in as the `name` prop */}
       {/* the Footer component needs a year passed in as the `year` prop */}
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
