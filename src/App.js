4401dcc3d68b6ef7988a143e1af33ecdbc15b850
DJ Drakos <dianajodrakos@gmail.com>
2022-01-24 14:42:38 -0800

    render all main components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css'
-{/* import the zodiac array */}
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Main from './Main/Main'
+{
+  /* import the zodiac array */
+}
 
 function App() {
   return (
     <div className="App">
       {/* import and use the Header. Main, and Footer components here */}
+      <Header />
+      <Main />
+      <Footer />
       {/* the Header component needs a string passed in as the `name` prop */}
       {/* the Footer component needs a year passed in as the `year` prop */}
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
